eb5f211c3c6f88f5278a7051bc292098e749a1fb
char16t <v.manenkov@gmail.com>
2015-12-19 01:03:07 +0300

    Force update dotfiles in install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install git
 sudo apt-get install ctags
-for f in `pwd`/files/.[^.]*; do ln -s $f ~/`basename $f`; done
+for f in `pwd`/files/.[^.]*; do ln -fs $f ~/`basename $f`; done
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c "PluginInstall"
