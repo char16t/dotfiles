7994fd3b6bdbbea2daff47caecae43a59c627da8
Valeriy Manenkov <v.manenkov@gmail.com>
2015-12-18 20:23:33 +0000

    Rename gitconfig to .gitconfig and vimrc to .vimrc

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install git
 sudo apt-get install ctags
-ln -s `pwd`/files/vimrc ~/.vimrc
-ln -s `pwd`/files/gitconfig ~/.gitconfig
+ln -s `pwd`/files/.vimrc ~/.vimrc
+ln -s `pwd`/files/.gitconfig ~/.gitconfig
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c "PluginInstall"
