5a370ec547b37d5d16ccc821b368cd05c58c8e22
Valeriy Manenkov <v.manenkov@gmail.com>
2016-01-23 23:05:18 +0300

    install.sh: Run ansible-playbook for playbook.yml

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install ansible
+ansible-playbook playbook.yml -i local
 for f in `pwd`/files/.[^.]*; do ln -fs $f ~/`basename $f`; done
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c "PluginInstall"
