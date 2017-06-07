38ef47b04a7e69a81f6d81da34a45e08e7999637
Valeriy Manenkov <v@manenkov.com>
2017-06-07 22:36:13 +0300

    Use package.el instead of deps sources

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -1,11 +1,25 @@
+;; packages
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("marmalade" . "http://marmalade-repo.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")))
+
+(require 'package)
+(package-initialize)
+
+(setq my-packages
+      '(
+	use-package
+	bind-key))
+
+(dolist (pkg my-packages)
+  (unless (package-installed-p pkg)
+    (package-refresh-contents)
+    (package-install pkg)))
+
 ;; Personal information
 (setq user-full-name "Valeriy Manenkov"
       user-mail-address "v.manenkov@gmail.com")
-
-;; Append Load Path and require packages
-(add-to-list 'load-path "~/.emacs.d/elisp/")
-(require 'bind-key)
-(require 'use-package)
 
 ;; Show column number
 (column-number-mode 1)
@@ -135,19 +149,6 @@
 (bind-key "C-c a" 'org-agenda)
 (bind-key "<f9> <f9>" 'org-agenda-list)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;; Clocktable By Tags
 (require 'org-table)
 (require 'org-clock)
@@ -199,4 +200,3 @@
 	      (clocktable-by-tag/insert-tag params))
 	    tags)))
 
-(provide 'clocktable-by-tag)
