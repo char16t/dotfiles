763f475bcf11c94f945f5ee37c4f56940aaffa8d
Valeriy Manenkov <valeriy@manenkov.com>
2023-12-22 12:01:29 +0300

    clean

--- setup/dotfiles-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dotfiles=(`find ${HOME}/dotfiles/files/* | grep -i "\.*$"`)
-dir="${HOME}/dotfiles/files"
-
-for dotfile in "${dotfiles[@]}";do
-    ln -sfv "${dotfile}" ~/.$(basename "${dotfile}")
-done
